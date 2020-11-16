4309dcd33cc0bbd5984fcb901ba75bdf53fa9f3e
anandhupvr <anandhupvr@gmail.com>
2020-11-16 04:59:25 -0800

    texture visualization

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,31 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {	
+
+	int _ViewpointF = 420;
+
+
+	pangolin::CreateWindowAndBind("Plane_Slam: Map Viewer",1024,768);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	pangolin::CreatePanel("menu").SetBounds(0.0,1.0,0.0,0.2);
+	pangolin::Var<bool> menu("menu.test",true,true);
+	pangolin::OpenGlRenderState s_cam(
+	pangolin::ProjectionMatrix(1024,768,_ViewpointF,_ViewpointF,512,389,0.1,1000),
+	pangolin::ModelViewLookAt(-2,2,-2, 0,0,0,0.0,-1.0, 0.0)
+	);
+
+	pangolin::View& d_cam = pangolin::CreateDisplay()
+			.SetBounds(0.0, 1.0, 0.2, 1.0, -1024.0f/768.0f)
+			.SetHandler(new pangolin::Handler3D(s_cam));
+
+	pangolin::View& rgb_image = pangolin::Display("rgb")
+	  .SetBounds(0, 0.2, 0.2, 0.4, 1024.0f/768.0f)
+	  .SetLock(pangolin::LockLeft, pangolin::LockBottom);
+
+
+
 	pangolin::GlTexture texture;
 	const GLenum internalFormat = GL_RGB;
 	const GLenum format = GL_BGR;
@@ -17,15 +42,28 @@ int main(int argc, char const *argv[])
 
 
 
-	cv::Mat img = cv::imread("muscle.jpg");
+	cv::Mat img = cv::imread("/home/developer/slam/src/muscle.jpg");
+	cv::resize(img, img, cv::Size(640, 480));
+
+	cout << img.size() << endl;
+
 	int width = img.cols;
 	int height = img.rows;
 
 
 	texture = pangolin::GlTexture(width, height, internalFormat, draw, 0, format, dataType);
-	texture.Upload(img.data,  GL_BGR, GL_UNSIGNED_BYTE);
 
-	// texture(new pangolin::GlTexture(width, height, internalFormat, draw, 0, format, dataType));
+	while(!pangolin::ShouldQuit())
+	{	
+
+		cout << texture.tid << endl;
+		texture.Upload(img.data,  GL_BGR, GL_UNSIGNED_BYTE);
+		rgb_image.Activate();
+		glColor3f(1.0,1.0,1.0);
+		texture.RenderToViewportFlipY();
+		pangolin::FinishFrame();
+	}
+
 
 	std::cout<<"yees";
 	return 0;
